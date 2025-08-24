dc8933da0c47eadda6a8e11bd3b14959e8fb855b
shahidur-rahman18 <mdshahidurrahman3690@gmail.com>
2025-08-24 15:16:16 +0600

    log in page done

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,24 @@
 document.getElementById('loginButton')
 .addEventListener('click', function(e) {
      e.preventDefault();
-    console.log('Login button clicked');
-    console.log(e);
+     const mobileNumber = 1234567890;
+     const pinNumber = 1234;
+     const mobileNumberValue=document.getElementById('mobile-number').value
+     const mobileNumberValueInt = parseInt(mobileNumberValue)
+     
+     
+     
+     const pinNumberValue=document.getElementById('pin-number').value
+     const pinNumberValueIntPin = parseInt(pinNumberValue)
+     console.log(mobileNumberValueInt,pinNumberValueIntPin)
+
+
+
+     if(mobileNumberValueInt === mobileNumber && pinNumberValueIntPin === pinNumber){
+        window.location.href = "./home.html"
+     }
+     else{
+        alert('Invalid Mobile Number or Pin')
+     }
+
 })
