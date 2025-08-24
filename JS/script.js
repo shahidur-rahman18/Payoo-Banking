abc7518d4cd639d31045edc9f1efbf2d1ab10286
shahidur-rahman18 <mdshahidurrahman3690@gmail.com>
2025-08-24 17:23:19 +0600

    home.html add

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,20 @@
-document.getElementById('loginButton')
-.addEventListener('click', function(e) {
-     e.preventDefault();
-     const mobileNumber = 1234567890;
-     const pinNumber = 1234;
-     const mobileNumberValue=document.getElementById('mobile-number').value
-     const mobileNumberValueInt = parseInt(mobileNumberValue)
-     
-     
-     
-     const pinNumberValue=document.getElementById('pin-number').value
-     const pinNumberValueIntPin = parseInt(pinNumberValue)
-     console.log(mobileNumberValueInt,pinNumberValueIntPin)
+document.getElementById("loginButton").addEventListener("click", function (e) {
+  e.preventDefault();
+  const mobileNumber = 1234567890;
+  const pinNumber = 1234;
+  const mobileNumberValue = document.getElementById("mobile-number").value;
+  const mobileNumberValueInt = parseInt(mobileNumberValue);
 
+  const pinNumberValue = document.getElementById("pin-number").value;
+  const pinNumberValueIntPin = parseInt(pinNumberValue);
+  console.log(mobileNumberValueInt, pinNumberValueIntPin);
 
-
-     if(mobileNumberValueInt === mobileNumber && pinNumberValueIntPin === pinNumber){
-        window.location.href = "./home.html"
-     }
-     else{
-        alert('Invalid Mobile Number or Pin')
-     }
-
-})
+  if (
+    mobileNumberValueInt === mobileNumber &&
+    pinNumberValueIntPin === pinNumber
+  ) {
+    window.location.href = "./home.html";
+  } else {
+    alert("Invalid Mobile Number or Pin");
+  }
+});
