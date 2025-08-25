0a12799b53428cddb1ff855fb855ff2c2074fa24
shahidur-rahman18 <mdshahidurrahman3690@gmail.com>
2025-08-25 18:15:24 +0600

    5 commit

--- JS/home.js
+++ JS/home.js
@@ -1,4 +1,5 @@
 const validPin=1234
+const  transactionData=[]
 
 // functon to get input value 
 function getInputValue(id){
@@ -73,6 +74,12 @@ document.getElementById('addmonery-button')
    const accountNumber=getInputValue('account-number')
 
    const amount = getInputValue('amount-add')
+
+    if(amount<=0){
+        alert("invalid amount")
+        return;
+    }
+
    const pinNumber = getInputValue('pin-number') 
    console.log(bank,accountNumber,amount,pinNumber);
    
@@ -89,6 +96,16 @@ document.getElementById('addmonery-button')
    const totalCurrentBalance=avaiableBalance+amount;
    setInnerText(totalCurrentBalance);
 
+//    database for add money
+ const data = {
+    name :'Add Money',
+    data: new Date().toLocaleString()
+
+ }
+  transactionData.push(data)
+    console.log(transactionData)
+
+
 })
 
 // cashout feature
@@ -100,42 +117,94 @@ document.getElementById('withdraw-button')
     console.log(amount);
 
     const avaiableBalance= getInnerText('avaiable-balance')
+     if(amount<=0 || amount>avaiableBalance){
+        alert("invalid amount")
+        return
+    }
 
      const totalCurrentBalance=avaiableBalance-amount;
     setInnerText(totalCurrentBalance);
 
+     const data = {
+        name:"Cash Out",
+        date:new Date().toLocaleTimeString()
+    }
+
+     transactionData.push(data)
+    console.log(transactionData) 
 
 
 })
+
+
+      document.getElementById('transactions-button').addEventListener("click",function(){
+    const transactionContainer = document.getElementById('transaction-container')
+    transactionContainer.innerText = ''
+
+    for(const data of transactionData){
+        const div = document.createElement("div")
+        div.innerHTML=`
+        <div class=" bg-white rounded-xl p-3 flex justify-between items-center mt-3">
+              <div class="flex items-center">
+                  <div class="p-3 rounded-full bg-[#f4f5f7]">
+                    <img src="./assets/wallet1.png" class="mx-auto" alt="" />
+                  </div>
+                  <div class="ml-3">
+                    <h1>${data.name}</h1>
+                    <p>${data.date}</p>
+                  </div>
+              </div>
+      
+              <i class="fa-solid fa-ellipsis-vertical"></i>
+            </div>
+        `
+
+        transactionContainer.appendChild(div)
+
+
+    }
+})
+  
+
+
 
  
 // togggle feature
 document.getElementById('add-money-btn')
 .addEventListener('click',function(){
-    toggleDisplay('cashout-parent')
+    toggleDisplay('add-money-parent')
    toggleDisplayEffect('add-money-btn')
 
 })
 
 document.getElementById('cashout-btn')
 .addEventListener('click',function(){
- toggleDisplay('add-money-parent')
+ toggleDisplay('cashout-parent')
    toggleDisplayEffect('cashout-btn')
 
 })
 document.getElementById('transfer-btn')
 .addEventListener('click',function(){
- toggleDisplay('cashout-parent')
+ toggleDisplay('transfer-money-parent')
    toggleDisplayEffect('transfer-btn')
 
 })
 document.getElementById('get-bonus-btn')
 .addEventListener('click',function(){
- toggleDisplay('transfer-money-parent')
+ toggleDisplay('get-bonus-parent')
    toggleDisplayEffect('get-bonus-btn')
 
 })
 
+
+document.getElementById('pay-bill-btn').addEventListener("click",function(){
+     toggleDisplay('pay-bill-parent')
+    toggleDisplayEffect('pay-bill-btn')
+})
+document.getElementById('transactions-button').addEventListener("click",function(){
+     toggleDisplay('transactions-parent')
+     toggleDisplayEffect('transactions-button')
+})
 
 
 
