ba101df86ee3a853c79112019cb33452312a8d6d
shahidur-rahman18 <mdshahidurrahman3690@gmail.com>
2025-08-25 11:09:12 +0600

    add functional

---
+++ JS/home.js
@@ -0,0 +1,26 @@
+const validPin=1234
+document.getElementById('addmonery-button')
+.addEventListener('click',function(e){
+    e.preventDefault();
+   const bank=document.getElementById('select-bank').value;
+   const accountNumber=document.getElementById('account-number').value;
+
+   const amount = parseInt(document.getElementById('amount-add').value);
+   const pinNumber = parseInt(document.getElementById('pin-number').value);
+   console.log(bank,accountNumber,amount,pinNumber);
+   
+   const avaiableBalance= parseInt(document.getElementById('avaiable-balance').innerText);
+
+   if(accountNumber.length<11){
+    alert('Account number must be 11 digit');
+    return;
+   }
+   if(pinNumber!==validPin){
+    alert('Invalid Pin number');
+    return;
+   }
+   const totalCurrentBalance=avaiableBalance+amount;
+   document.getElementById('avaiable-balance').innerText=totalCurrentBalance;
+  
+
+})
